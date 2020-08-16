a70cbe9a3dabbdc3790786a188df0da45bcd84ca
CodingPapi <codingpapi@gmail.com>
2020-08-16 13:12:53 +0800

    Extra test of ex5

--- machine-learning-ex5/ex5/learningCurve.m
+++ machine-learning-ex5/ex5/learningCurve.m
@@ -54,6 +54,13 @@ error_val   = zeros(m, 1);
 % ---------------------- Sample Solution ----------------------
 
 
+for i = 1:m
+  theta = trainLinearReg(X(1:i,:), y(1:i,:),lambda);
+  [Jtheta, grad] = linearRegCostFunction(X(1:i,:), y(1:i,:),theta,0);
+  error_train(i) = Jtheta;
+  [Jtheta, grad] = linearRegCostFunction(Xval, yval,theta,0);
+  error_val(i) = Jtheta;
+end
 
 
 
